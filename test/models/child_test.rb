dba3ead31f5b93bf04fb4dda76688aebee676f5b
Daniel Ng <danielng@andrew.cmu.edu>
2021-02-06 16:19:45 -0500

    child validation tests

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -5,7 +5,9 @@ class ChildTest < ActiveSupport::TestCase
   should have_many(:chores)
   should have_many(:tasks).through(:chores)
   
-  # TODO: Validation macros
+  # Validation macros
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
 
   # TODO: Context testing
 
