4afcb2c04e264dfd33c8efcd57f61158d5307624
Daniel Ng <danielng@andrew.cmu.edu>
2021-02-06 21:56:52 -0500

    task tests passed except the chore (same w/ child model)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ApplicationRecord
+
+  # Relationships
+  has_many :chores
+  has_many :children, through: :chores
+
+  # Validations
+  validates_presence_of :name
+  validates_numericality_of :points, only_integer: true, greater_than_or_equal_to: 0
+  
+  # Scopes
+  scope :alphabetical, -> { order("name") }
+  scope :active, -> { where(active: true) }
+
 end
