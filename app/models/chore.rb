ca94112576dbcd90a714b6c05b1c2f167c80ba8b
Daniel Ng <danielng@andrew.cmu.edu>
2021-02-06 22:24:06 -0500

    chore model done

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -4,4 +4,24 @@ class Chore < ApplicationRecord
   belongs_to :child
   belongs_to :task
 
+  # Validations
+  validates_date :due_on
+
+  # Scopes
+  scope :by_task, -> { joins(:task).order("name") }
+  scope :chronological, -> { order("due_on, completed") }
+  scope :pending, -> { where(completed: false) }
+  scope :done, -> { where(completed: true) }
+  scope :upcoming, -> { where("due_on >= ?", Date.current) }
+  scope :past, -> { where("due_on < ?", Date.current) }
+
+  # Methods
+  def status
+    if self.completed == true
+      "Completed"
+    else
+      "Pending"
+    end
+  end
+
 end
